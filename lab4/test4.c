b947aefdc4cd3c27f09afee4ce7c26e3929bd13f
ei12068 <ei12068@fe.up.pt>
2014-10-31 16:56:27 +0000

    Import of code provided for lab4

---
+++ lab4/test4.c
@@ -0,0 +1,17 @@
+#include "test4.h"
+
+int test_packet(unsigned short cnt){
+    /* To be completed ... */
+}
+
+int test_async(unsigned short idle_time) {
+    /* To be completed ... */
+}
+
+int test_config(void) {
+    /* To be completed ... */
+}
+
+int test_gesture(short length, unsigned short tolerance) {
+    /* To be completed ... */
+}
