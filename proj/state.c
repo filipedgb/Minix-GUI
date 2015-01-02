5c0e8ffbc5f80e884e20e7b9f9b8a4b7b519d60d
ei12068 <ei12068@fe.up.pt>
2015-01-02 02:17:33 +0000

    refactor 1.2

--- proj/state.c
+++ proj/state.c
@@ -1,5 +1,37 @@
 #include "state.h"
 
+void init() {
+	sef_startup();
+	subscribe_devices();
+	getSubFolders(".");
+
+	background = (char*) malloc (getVideoMemSize());
+	memcpy((char*)background,(char*) getBuffer(), getVideoMemSize());
+
+}
+
+void subscribe_devices() {
+	vg_init(0x105);
+
+	shift_mouse = mouse_subscribe_int();
+	shift_keyboard = keyboard_subscribe_int();
+	shift_timer = timer_subscribe_int();
+
+	kbc_input(KBC_WRITE_COMMAND);
+	issue_command_mouse(0xF6,-1);
+	kbc_input(KBC_WRITE_COMMAND);
+	issue_command_mouse(ENABLE_STREAM_MODE,-1);
+	kbc_input(KBC_WRITE_COMMAND);
+	issue_command_mouse(ENABLE_DATA_PACKETS,-1);
+}
+
+void unsubscribe_devices() {
+	keyboard_unsubscribe_int();
+	timer_unsubscribe_int();
+	mouse_unsubscribe_int();
+	vg_exit();
+}
+
 
 void updateScreen() {
 	cleanScreen();
@@ -22,55 +54,84 @@ void playIntro(){
 }
 
 
-void subscribe_devices() {
-	vg_init(0x105);
+void kbc_consequences(int output) {
 
-	shift_mouse = mouse_subscribe_int();
-	shift_keyboard = keyboard_subscribe_int();
-	shift_timer = timer_subscribe_int();
+	if(output == 2) {
+		enableBox(1,"Do you want to delete the selected files?");
+		setDeleteFlag();
+		updateScreen();
 
-	kbc_input(KBC_WRITE_COMMAND);
-	issue_command_mouse(0xF6,-1);
-	kbc_input(KBC_WRITE_COMMAND);
-	issue_command_mouse(ENABLE_STREAM_MODE,-1);
-	kbc_input(KBC_WRITE_COMMAND);
-	issue_command_mouse(ENABLE_DATA_PACKETS,-1);
+	}
+	else if (output == 3) {
+		navigateLeft();
+		updateScreen();
+	}
+
+	else if (output == 4) {
+		navigateRight();
+		updateScreen();
+	}
+	else if (output == 5) {
+		navigateUp();
+		updateScreen();
+	}
+	else if (output == 6) {
+		navigateDown();
+		updateScreen();
+	}
+	else if (output == 7) {
+		openFolderByEnter();
+		updateScreen();
+	}
+	else if (output == 8) {
+		moveBack();
+		updateScreen();
+	}
+
+
+
 
 }
 
-void init() {
-	sef_startup();
-	subscribe_devices();
-	getSubFolders(".");
+void mouse_consequences(int output) {
+	if(output == 2) { // S� h� verifica��es l�gicas no final de cada pacote (return 2 do handler)
 
-	background = (char*) malloc (getVideoMemSize());
-	memcpy((char*)background,(char*) getBuffer(), getVideoMemSize());
+		if(current_mouse_state.lb == 1) {
+			if(get_counter() > 25) {
+				check_mouse_click(current_mouse_state);
+			}
+			else {
+				check_mouse_double_click(current_mouse_state);
+			}
 
+			reset_counter();
+			updateScreen();
+		}
+		else if(current_mouse_state.rb == 1 && check_mouse_click(current_mouse_state) == 3) {
+			updateScreen();
+			drawRightClickMenu(current_mouse_state);
+			memcpy((char*)background, (char*) getBuffer(), getVideoMemSize());
+
+		}
+
+	}
 }
 
 
-void unsubscribe_devices() {
-	keyboard_unsubscribe_int();
-	timer_unsubscribe_int();
-	mouse_unsubscribe_int();
-	vg_exit();
-
-}
 
 
 int loop() {
-
+	int output;
 	int menu_open = 0;
-
 
 	int ipc_status,r, seconds = 0, running = 1;
 	message msg;
 	unsigned long code;
 
-	int esc_pressed = 0;
 	int ticker = 0;
 	int updated = 0;
 	int estado = 0;
+	int esc_pressed = 0;
 
 	updateScreen();
 
@@ -100,7 +161,7 @@ int loop() {
 				if (msg.NOTIFY_ARG & BIT(shift_keyboard)) { /* subscribed interrupt  bit 1 fica a 1, logo é 1*/
 					//	printf("KEYBOARD INTERRUPT\n");
 
-					int output = keyboard_int_handler_C(&code);
+					output = keyboard_int_handler_C(&code);
 
 					if(output == 1) {
 						if(esc_pressed) {
@@ -113,77 +174,16 @@ int loop() {
 						esc_pressed = 1;
 					}
 
-					else if(output == 2) {
-						enableBox(1,"Do you want to delete the selected files?");
-						setDeleteFlag();
-						updateScreen();
+					kbc_consequences(output);
 
-					}
-					else if (output == 3) {
-						navigateLeft();
-						updateScreen();
-					}
-
-					else if (output == 4) {
-						navigateRight();
-						updateScreen();
-					}
-					else if (output == 5) {
-						navigateUp();
-						updateScreen();
-					}
-					else if (output == 6) {
-						navigateDown();
-						updateScreen();
-					}
-					else if (output == 7) {
-						openFolderByEnter();
-						updateScreen();
-					}
-					else if (output == 8) {
-						moveBack();
-						updateScreen();
-					}
 				}
 
 				else if(msg.NOTIFY_ARG & BIT(shift_mouse)) {
 					printf("MOUSE INTERRUPT\n");
 					//cleanCursor(current_mouse_state);
 
-					int output = mouse_int_handler(&current_mouse_state);
-
-					if(output == 2) { // S� h� verifica��es l�gicas no final de cada pacote (return 2 do handler)
-
-						if(current_mouse_state.lb == 1) {
-							if(get_counter() > 25) {
-								if(check_mouse_click(current_mouse_state) == 1) {
-
-
-								}
-
-								else if(check_mouse_click(current_mouse_state) == 4) {
-
-
-								}
-							}
-							else {
-								check_mouse_double_click(current_mouse_state);
-							}
-
-							reset_counter();
-							updateScreen();
-
-
-						}
-
-						else if(current_mouse_state.rb == 1 && check_mouse_click(current_mouse_state) == 3) {
-							updateScreen();
-							drawRightClickMenu(current_mouse_state);
-							memcpy((char*)background, (char*) getBuffer(), getVideoMemSize());
-
-						}
-
-					}
+					output = mouse_int_handler(&current_mouse_state);
+					mouse_consequences(output);
 
 
 					updated = 1;
