9674be2fd5c036daf47060147900ab3d28138d22
ei12068 <ei12068@fe.up.pt>
2015-01-02 19:15:39 +0000

    sem warnings

--- proj/state.c
+++ proj/state.c
@@ -212,6 +212,51 @@ int loop() {
 	}
 
 	return 1;
-
-
 }
+
+
+
+
+
+
+void minixBootAnimation(unsigned int time) {
+
+	vg_init(0x105);
+
+	char**sprites[5] = {loading1, loading2, loading3, loading4, loading5};
+
+	int shift_timer = timer_subscribe_int();
+
+	int ipc_status,r, seconds = 0, running = 1;
+	message msg;
+
+	int index = 0;
+	mainDraw();
+
+	while(get_seconds()  < time) {
+
+		if ( driver_receive(ANY, &msg, &ipc_status) != 0 ) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { // received notification
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: // hardware interrupt notification
+				if (msg.NOTIFY_ARG & BIT(shift_timer)) {
+					timer_int_handler();
+					if(index > 4) break; else draw_sprite(centerImageX(135), 300, sprites[index]);
+					if(index != 0) sleep(1);
+				}
+			default:
+				break;
+			}
+		}
+		flipDisplay();
+		index++;
+	}
+	timer_unsubscribe_int();
+}
+
+
+
+
